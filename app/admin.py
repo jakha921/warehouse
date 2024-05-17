52758d6f19ea37cea98322ff714241f53a02eda2
jakha921 <jakha921@gmail.com>
2024-05-17 12:06:22 +0500

    first commit

---
+++ app/admin.py
@@ -0,0 +1,50 @@
+from django.contrib import admin
+
+from app.models import Reception, Product, Warehouse, Transmitting
+
+# Register your models here.
+admin.site.site_header = 'Mahsulotlar ombori'
+admin.site.site_title = 'Mahsulotlar ombori'
+admin.site.index_title = 'Mahsulotlar ombori'
+
+
+@admin.register(Reception)
+class ReceptionAdmin(admin.ModelAdmin):
+    list_display = ['product_name', 'product_count', 'product_price', 'sender', 'receiver', 'receiver_date']
+    search_fields = ['product_name', 'sender', 'receiver']
+    list_filter = ['receiver_date']
+    ordering = ['-receiver_date']
+    list_per_page = 15
+    list_max_show_all = 100
+    date_hierarchy = 'receiver_date'
+    readonly_fields = ['receiver_date']
+    list_editable = ['product_price']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    ordering = ['name']
+    list_per_page = 15
+    list_max_show_all = 100
+
+
+@admin.register(Warehouse)
+class WarehouseAdmin(admin.ModelAdmin):
+    list_display = ['product', 'count']
+    search_fields = ['product']
+    list_filter = ['product']
+    ordering = ['product']
+    list_per_page = 15
+    list_max_show_all = 100
+
+
+@admin.register(Transmitting)
+class TransmittingAdmin(admin.ModelAdmin):
+    list_display = ['product', 'count']
+    search_fields = ['product']
+    list_filter = ['product']
+    ordering = ['product']
+    list_per_page = 15
+    list_max_show_all = 100
---
+++ app/models.py
@@ -0,0 +1,91 @@
+from django.db import models
+
+
+# Create your models here.
+class Reception(models.Model):
+    product_name = models.CharField(max_length=100, verbose_name='Mahsulot nomi')
+    product_count = models.IntegerField(verbose_name='Mahsulot soni')
+    product_price = models.FloatField(verbose_name='Mahsulot narxi', blank=True, null=True)
+    sender = models.CharField(max_length=100, verbose_name='Jo\'natuvchi')
+    receiver = models.CharField(max_length=100, verbose_name='Qabul qiluvchi')
+    receiver_date = models.DateField(verbose_name='Qabul qilingan sana')
+
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.product_name} - {self.product_count}'
+
+    class Meta:
+        verbose_name = 'Qabul qilingan mahsulot'
+        verbose_name_plural = 'Qabul qilingan mahsulotlar'
+        ordering = ['-receiver_date', 'product_name']
+        db_table = 'reception'
+        indexes = [
+            models.Index(fields=['receiver_date', 'product_name']),
+        ]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Mahsulot nomi')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Mahsulot'
+        verbose_name_plural = 'Mahsulotlar'
+        ordering = ['name']
+        db_table = 'product'
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+
+
+class Warehouse(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Mahsulot')
+    count = models.IntegerField(verbose_name='Mahsulot soni')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.product} - {self.count}'
+
+    class Meta:
+        verbose_name = 'Ombor'
+        verbose_name_plural = 'Omborlar'
+        ordering = ['product']
+        db_table = 'warehouse'
+        indexes = [
+            models.Index(fields=['product']),
+        ]
+
+
+class Transmitting(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Mahsulot')
+    count = models.IntegerField(verbose_name='Mahsulot soni')
+    receiver = models.CharField(max_length=100, verbose_name='Qabul qiluvchi')
+    receiver_date = models.DateField(verbose_name='Qabul qilingan sana')
+    comment = models.TextField(blank=True, null=True, verbose_name='Izoh')
+    department = models.CharField(max_length=100, verbose_name='Bo\'lim', blank=True, null=True)
+
+    image = models.ImageField(upload_to='transmitting/', blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.product} - {self.count}'
+
+    class Meta:
+        verbose_name = 'Jo\'natilgan mahsulot'
+        verbose_name_plural = 'Jo\'natilgan mahsulotlar'
+        ordering = ['-receiver_date', 'product']
+        db_table = 'transmitting'
+        indexes = [
+            models.Index(fields=['receiver_date', 'product']),
+        ]
